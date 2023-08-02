bdac670f243654af984551a622d6bc35d92b30c4
Tijesunimi <otegbolamarvellous@gmail.com>
2023-08-02 20:39:04 +0100

    Minor changes for consistency

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,7 @@
 import random
 from time import sleep
 
+
 def rps(bot_name, player_name):
     # Welcome the user to the game
     print("Welcome to rock, paper, scissors. Good luck!")
@@ -13,7 +14,6 @@ def rps(bot_name, player_name):
         rounds = input("Enter the number of rounds you want to play: ")
         if not rounds.isdecimal():
             print("Enter valid number of rounds")
-            
 
         elif int(rounds) % 2 == 0:
             print("Rounds must be an odd number (1,3,5,...)")
@@ -26,41 +26,41 @@ def rps(bot_name, player_name):
         print(f"Round #{round + 1}")
 
         # Make a choice for the computer player
-        computer_choice = random.choice(["r", "p", "s"])
+        bot_choice = random.choice(["r", "p", "s"])
 
         # Get a choice from the user
         player_choice = input("Rock (r), Paper (p) or Scissors (s)? ")
 
         # Printing what the computer chooses
-        print(f"The computer chooses {computer_choice}")
+        print(f"{bot_name} chooses {bot_choice}")
 
         # Compare the user and computer choice
         # Print the right message, based on the choices
-        if (computer_choice == "r" and player_choice == "p"):
+        if (bot_choice == "r" and player_choice == "p"):
             print(f"Paper covers Rock, {player_name} wins this round")
             player_score += 1
 
-        elif (computer_choice == "p" and player_choice == "r"):
+        elif (bot_choice == "p" and player_choice == "r"):
             print(f"Paper covers Rock, {bot_name} wins this round")
             bot_score += 1
 
-        elif (computer_choice == "p" and player_choice == "s"):
+        elif (bot_choice == "p" and player_choice == "s"):
             print(f"Scissors cut Paper, {player_name} wins this round")
             player_score += 1
 
-        elif (computer_choice == "s" and player_choice == "p"):
+        elif (bot_choice == "s" and player_choice == "p"):
             print(f"Scissors cut Paper, {bot_name} wins this round")
             bot_score += 1
 
-        elif (computer_choice == "s" and player_choice == "r"):
+        elif (bot_choice == "s" and player_choice == "r"):
             print(f"Rock Smashes Scissors, {player_name} wins this round")
             player_score += 1
 
-        elif (computer_choice == "r" and player_choice == "s"):
+        elif (bot_choice == "r" and player_choice == "s"):
             print(f"Rock smashes Scissors, {bot_name} wins this round")
             bot_score += 1
 
-        elif computer_choice == player_choice:
+        elif bot_choice == player_choice:
             print("It's a draw")
 
     sleep(2)
@@ -77,4 +77,4 @@ def rps(bot_name, player_name):
         sleep(2)
         play_again = input("Do you want to play again? Y/N")
         if play_again.lower() == "y":
-            rps()       
+            rps()
